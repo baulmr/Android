418524155a703e70f58a0a3d115735e04ac0388b
kelvinsenduk <kelvinsenduk@gmail.com>
2019-01-18 16:16:31 +0700

    Activity DaftarKelompok dan Activity BuatKelompok

---
+++ app/src/main/java/com/example/acer/randomapplication/BuatKelompok.java
@@ -0,0 +1,13 @@
+package com.example.acer.randomapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BuatKelompok extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buat_kelompok);
+    }
+}
---
+++ app/src/main/java/com/example/acer/randomapplication/DaftarKelompok.java
@@ -0,0 +1,13 @@
+package com.example.acer.randomapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DaftarKelompok extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daftar_kelompok);
+    }
+}
